d520f973607ba1402a0418fa640c99eb59c82281
John <jonnyuz99@gmail.com>
2019-05-12 19:48:41 +0300

    basic header

--- Game/head.h
+++ Game/head.h
@@ -4,18 +4,25 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
 #include <cmath>
+#include <cstdlib>
 
 #include "objects.h"
 
-#define SCREEN_WIDTH 1340
-#define SCREEN_HIGHT 720
+#define SCREEN_WIDTH 	1340
+#define SCREEN_HEIGHT 	720
+#define ZERO			0
 
-#define FAST_TIME(ARG) (ARG).asSeconds() * 100
+#define SPEED 			100
 
-#define EARTH_RADIUS 70
-#define EARTH_X 670
-#define EARTH_Y 360
+#define EARTH_RADIUS 	70
+#define EARTH_X 		670
+#define EARTH_Y 		360
 
-int trashMove(Trash &, sf::Time &);
+#define STORE_SIZE 		64
+
+#define FAST_TIME(ARG) (ARG).asSeconds() * SPEED
+
+int trashMove		(Trash &, sf::Time &);
+void storeCreate	(std::vector<Trash> &);
 
 #endif
