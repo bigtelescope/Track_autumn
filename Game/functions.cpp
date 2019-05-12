cbd786d24e51b8190248c13cee06dc55c208d3bd
John <jonnyuz99@gmail.com>
2019-05-12 19:46:54 +0300

    main functions

--- Game/functions.cpp
+++ Game/functions.cpp
@@ -2,8 +2,8 @@
 
 int trashMove(Trash & trash, sf::Time & time)
 {
-	if(abs(trash.getCircleX() - EARTH_X) < EARTH_RADIUS &&
-	   abs(trash.getCircleY() - EARTH_Y) < EARTH_RADIUS)
+	if(abs(trash.getCircleX() + TRASH_RADIUS - EARTH_X) < EARTH_RADIUS &&
+	   abs(trash.getCircleY() + TRASH_RADIUS - EARTH_Y) < EARTH_RADIUS)
 	{
 		trash.killObject();
 		return 0;
@@ -18,4 +18,21 @@ int trashMove(Trash & trash, sf::Time & time)
 								  trash.getY() + (delta_y / radius) * FAST_TIME(time));
 	
 	return 0;
+}
+
+void storeCreate(std::vector<Trash> & vector)
+{
+	for(int i = 0; i < STORE_SIZE / 4; i++)
+	{
+		Trash trash_one(rand()%SCREEN_WIDTH, ZERO		  		);
+		Trash trash_two(rand()%SCREEN_WIDTH, SCREEN_HEIGHT 		);
+
+		Trash trash_three(ZERO,				 rand()%SCREEN_HEIGHT);
+		Trash trash_four (SCREEN_WIDTH,		 rand()%SCREEN_HEIGHT);
+
+		vector.push_back(trash_one);
+		vector.push_back(trash_two);
+		vector.push_back(trash_three);
+		vector.push_back(trash_four);
+	}
 }
