71d2bcf90e392f0240654be157bcd4cbe365906e
John <jonnyuz99@gmail.com>
2019-05-12 19:48:56 +0300

    lol

--- Game/main.cpp
+++ Game/main.cpp
@@ -2,21 +2,14 @@
 
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(SCREEN_WIDTH, SCREEN_HIGHT), "Work mf");
+	sf::RenderWindow window(sf::VideoMode(SCREEN_WIDTH, SCREEN_HEIGHT), "Work mf");
 	sf::Clock clock;
 	sf::Time time;
 
 	std::vector<Trash> trash_vector;
+	storeCreate(trash_vector);
 
-	Trash trash1(1340,590);
-	Trash trash2(0, 0);
-	Trash trash3(1340, 90);
-	Trash trash4(310, 10);
-
-	trash_vector.push_back(trash1);
-	trash_vector.push_back(trash2);
-	trash_vector.push_back(trash3);
-	trash_vector.push_back(trash4);
+	int alive_numb = 0;
 
 	sf::CircleShape earth(EARTH_RADIUS);
 	earth.setFillColor(sf::Color::Green);
@@ -26,26 +19,26 @@ int main()
 	{
 		sf::Event main_event;
 
-		time = clock.getElapsedTime();
-
-		for(int i(0); i < trash_vector.size(); i++)
-			trashMove(trash_vector[i], time);
-		
 		while(window.pollEvent(main_event))
 		{
 			if(main_event.type == sf::Event::Closed)
 				window.close();
 		}
 
+		time = clock.getElapsedTime();
+
+		trashMove(trash_vector[alive_numb], time);
+
 		window.clear(sf::Color::White);
 		window.draw(earth);
 
-		//	its acceptably to delete 31 and 32 strings and change "&& trash_vector[i].isAlive()"
-		//	to "trashMove(trash_vector[i], time)"
-		//
-		//  some kind of optimization:)
-		for(int i(0); i < trash_vector.size() && trash_vector[i].isAlive(); i++)
-				window.draw(trash_vector[i].getCircle());
+		if(trash_vector[alive_numb].isAlive())
+			window.draw(trash_vector[alive_numb].getCircle());
+		else
+		{
+			alive_numb = (alive_numb + 1) % STORE_SIZE;
+			clock.restart();
+		}
 
 		window.display();
 	}
