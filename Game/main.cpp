62c3305b3e32de08394150e8a6f569dca47713ea
John <jonnyuz99@gmail.com>
2019-05-20 15:34:56 +0300

    work with pictures

--- Game/main.cpp
+++ Game/main.cpp
@@ -3,30 +3,54 @@
 int main()
 {
 	sf::RenderWindow window(sf::VideoMode(SCREEN_WIDTH, SCREEN_HEIGHT), "Work mf");
-	sf::Clock clock, clock_sat, clock_bullet;
+
+	sf::Clock clock;
+	sf::Clock clock_sat;
+	sf::Clock clock_bullet;
+
 	sf::Time time;
+
+	sf::Event main_event;
+
 	sf::Vector2i mousePosition;
+
+	//temporary volatile
+	int alive_numb = 0;
+
+	sf::Texture texture;
+	texture.loadFromFile("pictures/meteor.png");
 
 	//creating vector of trash
 	std::vector<Trash> trash_vector;
-	storeCreate(trash_vector);
+	storeCreate(trash_vector, texture);
+/*
+	for(int j = 0; j < trash_vector.size(); j++)
+		trash_vector[j].getSprite().setTexture(texture);
+*/
 
 	//creating vector of bullets
 	//std::vector<Bullet> bullet_vector;
 	//bullet_storeCreate(bullet_vector);
 
+	// Satellite creating
+	Satellite satellite(SATELLITE_START_X, SATELLITE_START_Y, SCALE);
 
-	int alive_numb = 0;
+	// Earth creating
+	Earth earth(EARTH_SPRITE_X, EARTH_SPRITE_Y, EARTH_SCALE);
 
-	//earth creating
-	sf::CircleShape earth(EARTH_RADIUS);
-	earth.setFillColor(sf::Color::Green);
-	earth.setPosition(EARTH_X - EARTH_RADIUS, EARTH_Y - EARTH_RADIUS);
+	sf::Texture background_texture;
+	sf::Sprite 	background;
 
-	//satellite creating
-	Satellite satellite(SATELLITE_START_X, SATELLITE_START_Y);
+	background_texture.loadFromFile("pictures/background.png");
+  	
+  	sf::Vector2u texture_size = background_texture.getSize(); //Get size of texture.
+    sf::Vector2u window_size  = window.getSize();             //Get size of window.
 
-	sf::Event main_event;
+    float scale_x = (float)window_size.x / texture_size.x;	  //Calculate scale.
+    float scale_y = (float)window_size.y / texture_size.y;    //Calculate scale.
+
+    background.setTexture(background_texture);
+    background.setScale	 (scale_x, scale_y);
 
 	while(window.isOpen())
 	{
@@ -63,17 +87,19 @@ int main()
 		satelliteMove(satellite, clock_sat);
 
 		window.clear(sf::Color::White);
+		window.draw(background);
 		
-
 		if(trash_vector[alive_numb].isAlive())
-			window.draw(trash_vector[alive_numb].getCircle());
+			window.draw(trash_vector[alive_numb].getSprite());
 		else
 		{
 			alive_numb = (alive_numb + 1) % STORE_SIZE;//move to the beginning
 			clock.restart();
 		}
-		window.draw(earth);
-		window.draw(satellite.getCircle());
+
+		window.draw(satellite.getSprite());
+		window.draw(earth.getSprite());
+
 		window.display();
 	}
 	return 0;
